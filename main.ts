b8f46e98f1fe2a0b90d3ee595a5bd8bf5f3cdb21
martinniee <1954924950@qq.com>
2022-09-28 19:50:05 +0800

    阶段保存

--- main.ts
+++ main.ts
@@ -51,13 +51,44 @@ export default class MyPlugin extends Plugin {
 		this.register(
 			this.onElement(
 				document,
-				"mouseover" as keyof HTMLElementEventMap,
-				"img",
+				"mouseenter" as keyof HTMLElementEventMap,
+				".internal-embed image-embed is-loaded",
 				this.onMouseover.bind(this)
+			)
+		);
+		this.register(
+			this.onElement(
+				document,
+				"mouseleave" as keyof HTMLElementEventMap,
+				".internal-embed image-embed is-loaded",
+				()=>{
+					this.deleteButton();
+
+					// console.log("鼠标移出")
+					// if(this.isMouseoutButton()){
+						// const btn_delete = document.querySelector(".btn-delete") as HTMLButtonElement;
+						// btn_delete.parentElement?.removeChild(btn_delete);
+
+					// }else{
+						// console.log("鼠标移入到按钮");
+					// }
+				}
 			)
 		);
 	}
 	onunload() {}
+	/**
+	 * 用于判断鼠标是否移出删除按钮
+	 * @returns 
+	 */
+	isMouseoutButton = (): boolean=>{
+		const btn_delete = document.querySelector(".btn-delete") as HTMLButtonElement;
+		btn_delete.onmouseover = function(){
+			console.log("鼠标移入到按钮上了");
+			return false;
+		}
+		return true;
+	}
 
 	/**
 	 * 
@@ -71,15 +102,16 @@ export default class MyPlugin extends Plugin {
 	 * 删除 “删除图片按钮”
 	 */
 	deleteButton = ()=>{
-		const btn_delete: HTMLButtonElement = document.querySelector(".btn-delete") as HTMLButtonElement;
+		const btn_delete = document.querySelector(".btn-delete") as HTMLButtonElement;
+		console.log("btn_delete:	" + btn_delete)
 		btn_delete.parentElement?.removeChild(btn_delete);
 	}
 	/**
 	 * 用于获取动态生成的删除按钮 btn_delete
 	 * @returns 删除按钮元素的对象
 	 */
-	getDynamicDeleteButton = (): HTMLElement => {
-		const btn_delete: HTMLElement = document.createElement("button");
+	getDynamicDeleteButton = (): HTMLButtonElement => {
+		const btn_delete: HTMLButtonElement = document.createElement("button");
 		btn_delete.setAttribute("class","btn-delete");
 		// 将svg作为按钮子元素，并作为按钮的图标显示
 		btn_delete.innerHTML ='<svg fill="#ff0000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="48px" height="48px"><path d="M 28 7 C 25.243 7 23 9.243 23 12 L 23 15 L 13 15 C 11.896 15 11 15.896 11 17 C 11 18.104 11.896 19 13 19 L 15.109375 19 L 16.792969 49.332031 C 16.970969 52.510031 19.600203 55 22.783203 55 L 41.216797 55 C 44.398797 55 47.029031 52.510031 47.207031 49.332031 L 48.890625 19 L 51 19 C 52.104 19 53 18.104 53 17 C 53 15.896 52.104 15 51 15 L 41 15 L 41 12 C 41 9.243 38.757 7 36 7 L 28 7 z M 28 11 L 36 11 C 36.552 11 37 11.449 37 12 L 37 15 L 27 15 L 27 12 C 27 11.449 27.448 11 28 11 z M 19.113281 19 L 44.886719 19 L 43.212891 49.109375 C 43.153891 50.169375 42.277797 51 41.216797 51 L 22.783203 51 C 21.723203 51 20.846109 50.170328 20.787109 49.111328 L 19.113281 19 z M 32 23.25 C 31.033 23.25 30.25 24.034 30.25 25 L 30.25 45 C 30.25 45.966 31.033 46.75 32 46.75 C 32.967 46.75 33.75 45.966 33.75 45 L 33.75 25 C 33.75 24.034 32.967 23.25 32 23.25 z M 24.642578 23.251953 C 23.677578 23.285953 22.922078 24.094547 22.955078 25.060547 L 23.652344 45.146484 C 23.685344 46.091484 24.462391 46.835938 25.400391 46.835938 C 25.421391 46.835938 25.441891 46.835938 25.462891 46.835938 C 26.427891 46.801938 27.183391 45.991391 27.150391 45.025391 L 26.453125 24.939453 C 26.419125 23.974453 25.606578 23.228953 24.642578 23.251953 z M 39.355469 23.251953 C 38.388469 23.224953 37.580875 23.974453 37.546875 24.939453 L 36.849609 45.025391 C 36.815609 45.991391 37.571109 46.801938 38.537109 46.835938 C 38.558109 46.836938 38.578609 46.835938 38.599609 46.835938 C 39.537609 46.835938 40.314656 46.091484 40.347656 45.146484 L 41.044922 25.060547 C 41.078922 24.094547 40.321469 23.285953 39.355469 23.251953 z" /></svg>';
@@ -151,24 +183,6 @@ export default class MyPlugin extends Plugin {
 		// return currentImageFile;
 	};
 
-	/**
-	 * 注册鼠标移出
-	 * @param img
-	 * @param document
-	 */
-	registerMouseout(img: HTMLImageElement, document: Document = activeDocument) {
-		this.register(
-			this.onElement(
-				document,
-				"mouseout" as keyof HTMLElementEventMap,
-				"img",
-				(e: MouseEvent) => {
-					console.log("鼠标移出去到图片:");
-					this.deleteButton();
-				}
-			)
-		);
-	}
 
 	/**
 	 * 鼠标移入
@@ -180,6 +194,7 @@ export default class MyPlugin extends Plugin {
 		// event.target 获取鼠标事件的目标（图片元素）
 		const target = event.target as Element;
 		const imgType = target.localName;
+		console.log("🚩--------imgType: --------" + imgType)
 		let fileList: TFile[];
 		switch (imgType) {
 			// 当事件作用的目标元素是img标签时
@@ -191,10 +206,10 @@ export default class MyPlugin extends Plugin {
 				fileList = this.getFileObjectViaFullURLOfCurrentImage(image);
 				const Proto = thisURL.protocol;
 				// 🚩--------获取动态的图片删除按钮--------
-				const btn_delete: HTMLElement = this.getDynamicDeleteButton();
+				const btn_delete = this.getDynamicDeleteButton();
 				console.log("btn_delete:  " + btn_delete);
 				// 将动态生成的按钮添加到img元素的父级div元素下作为第一个子元素
-				imageDOM.parentElement?.insertBefore(btn_delete,imageDOM);
+				imageDOM.parentElement?.insertBefore(btn_delete as HTMLButtonElement,imageDOM);
 				// 当图片元素的协议是以下几种，则开始执行删除操作
 				switch (Proto) {
 					case "app:":
@@ -202,7 +217,7 @@ export default class MyPlugin extends Plugin {
 					case "http:":
 					case "https:":
 						// 给当前图片删除按钮添加点击监听事件
-						btn_delete.addEventListener("click", 
+						btn_delete?.addEventListener("click", 
 						async ()=>{
 							try {
 							// 获取图片元素的父级div的父级div
@@ -226,8 +241,15 @@ export default class MyPlugin extends Plugin {
 				}
 				break;
 			}
+			case "div": {
+				// 当目标是div时候
+				// 当元素是包含图片的div时
+				console.log("div:	" + target.attributes);
+				return;
+
+			}
 			default:
-				new Notice("No handler for this image type!");
+				// new Notice("No handler for this image type!");
 				return;
 		}
 
