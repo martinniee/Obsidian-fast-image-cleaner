5d203450dc0e96a47189adfe8c5d3a0e97c426a1
martinniee <1954924950@qq.com>
2023-02-09 15:20:52 +0800

    fix

--- src/modals.ts
+++ src/modals.ts
@@ -32,22 +32,22 @@ export class LogsModal extends Modal {
         const { contentEl } = this;
         const myModal = this;
 
-        // 创建一个窗口
+        // --------创建一个窗口--------
         const headerWrapper = contentEl.createEl('div');
         headerWrapper.addClass('fast-image-cleaner-center-wrapper');
        
-        // 创建一个h1用于显示标题
+        // --------创建一个h1用于显示标题--------
         const headerEl = headerWrapper.createEl('h1', { text: ' Detection of multiple image reference links - logs ' });
         headerEl.addClass('modal-title');
        
 
-        // 创建一个输入框
+        // --------创建一个日志内容框--------
         const logs = contentEl.createEl('div');
-        logs.addClass('.fast-image-cleaner-log');
+        logs.addClass('fast-image-cleaner-log');
         logs.innerHTML = this.getLog();
         
 
-        // 创建两个按钮,确定和取消
+        // ----------创建两个按钮,确定和取消---------------
         const buttonWrapper = contentEl.createEl('div');
         buttonWrapper.addClass('fast-image-cleaner-center-wrapper');
 
@@ -56,6 +56,7 @@ export class LogsModal extends Modal {
 
 
         closeButton.addClass('unused-images-button');
+        closeButton.setAttribute("aria-label","close the window");
         removeLinkButton.addClass('unused-images-button');
         removeLinkButton.setAttribute("aria-label","Continue to remove the reference link to the current image in the current document");
         closeButton.addEventListener('click', () => {
--- src/util.ts
+++ src/util.ts
@@ -14,7 +14,10 @@ export const getAllImgDivs = (): HTMLCollection => {
     // 2.得 实时预览模式 下 内容区div preview_content (div markdown-source-view cm-s-obsidian mod-cm6 is-readable-line-width is-folding is-live-preview node-insert-event)
     const preview_content: HTMLCollection = leaf_active[0].getElementsByClassName("markdown-source-view cm-s-obsidian mod-cm6 is-folding is-live-preview node-insert-event");
     // 3.得 内容区下所有图片附件div embed_divs
-    const embed_divs: HTMLCollection = preview_content[0].getElementsByClassName("internal-embed media-embed image-embed is-loaded");
+    const embed_divs: HTMLCollection = preview_content[0]?.getElementsByClassName("internal-embed media-embed image-embed is-loaded");
+    if( embed_divs == undefined || embed_divs == null){
+        new Notice('cannot get image element');
+    }
     return embed_divs;
 };
 /**
@@ -116,7 +119,7 @@ export const addDelBtn = (img_list: HTMLCollection) => {
         const btn_del: HTMLButtonElement = document.createElement("button");
         // 1、动态生成删除按钮 btn_del
         btn_del.setAttribute("class", "btn-delete");
-        btn_del.setAttribute("aria-label","删除图片");
+        btn_del.setAttribute("aria-label","Delete current image");
         // 将svg作为按钮子元素，并作为按钮的图标显示
         btn_del.innerHTML = '<svg fill="#ff0000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="48px" height="48px"><path d="M 28 7 C 25.243 7 23 9.243 23 12 L 23 15 L 13 15 C 11.896 15 11 15.896 11 17 C 11 18.104 11.896 19 13 19 L 15.109375 19 L 16.792969 49.332031 C 16.970969 52.510031 19.600203 55 22.783203 55 L 41.216797 55 C 44.398797 55 47.029031 52.510031 47.207031 49.332031 L 48.890625 19 L 51 19 C 52.104 19 53 18.104 53 17 C 53 15.896 52.104 15 51 15 L 41 15 L 41 12 C 41 9.243 38.757 7 36 7 L 28 7 z M 28 11 L 36 11 C 36.552 11 37 11.449 37 12 L 37 15 L 27 15 L 27 12 C 27 11.449 27.448 11 28 11 z M 19.113281 19 L 44.886719 19 L 43.212891 49.109375 C 43.153891 50.169375 42.277797 51 41.216797 51 L 22.783203 51 C 21.723203 51 20.846109 50.170328 20.787109 49.111328 L 19.113281 19 z M 32 23.25 C 31.033 23.25 30.25 24.034 30.25 25 L 30.25 45 C 30.25 45.966 31.033 46.75 32 46.75 C 32.967 46.75 33.75 45.966 33.75 45 L 33.75 25 C 33.75 24.034 32.967 23.25 32 23.25 z M 24.642578 23.251953 C 23.677578 23.285953 22.922078 24.094547 22.955078 25.060547 L 23.652344 45.146484 C 23.685344 46.091484 24.462391 46.835938 25.400391 46.835938 C 25.421391 46.835938 25.441891 46.835938 25.462891 46.835938 C 26.427891 46.801938 27.183391 45.991391 27.150391 45.025391 L 26.453125 24.939453 C 26.419125 23.974453 25.606578 23.228953 24.642578 23.251953 z M 39.355469 23.251953 C 38.388469 23.224953 37.580875 23.974453 37.546875 24.939453 L 36.849609 45.025391 C 36.815609 45.991391 37.571109 46.801938 38.537109 46.835938 C 38.558109 46.836938 38.578609 46.835938 38.599609 46.835938 C 39.537609 46.835938 40.314656 46.091484 40.347656 45.146484 L 41.044922 25.060547 C 41.078922 24.094547 40.321469 23.285953 39.355469 23.251953 z" /></svg>';
         // 2、依次将按钮渲染到图片区域
