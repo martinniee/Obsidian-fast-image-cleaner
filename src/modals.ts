920f900ec31d4b7f898a04feb93ecb2ccd5dbe92
martinniee <1954924950@qq.com>
2023-02-09 13:55:40 +0800

    修改，删除不需要的 窗口信息 和 css代码

--- src/modals.ts
+++ src/modals.ts
@@ -16,10 +16,10 @@ export class LogsModal extends Modal {
 
     getLog(): string{
         const fistEle: string = this.textToView.shift() as string;
-        const cur_md: string = "当前引用该图片的md文档( The md document that currently references the image: ) \n" + fistEle + "\n\n";
+        const cur_md: string = "The md document that currently references the image: </br>" + fistEle + "</br></br>";
 
-        let other_md: string = this.textToView.join("\n");
-        other_md = "全部引用该图片的文档列表(List of all documents that reference this image: \n" + other_md;
+        let other_md: string = this.textToView.join("</br>");
+        other_md = "List of all documents that reference this image: </br>" + other_md;
         const log: string = cur_md + other_md;
         return log;
     }
@@ -33,22 +33,21 @@ export class LogsModal extends Modal {
         headerWrapper.addClass('fast-image-cleaner-center-wrapper');
        
         // 创建一个h1用于显示标题
-        const headerEl = headerWrapper.createEl('h1', { text: '检测到该图片被多个文档同时引用,引用该图片的markdown文档列表如下 :' });
+        const headerEl = headerWrapper.createEl('h1', { text: ' Detection of multiple image reference links - logs ' });
         headerEl.addClass('modal-title');
        
-        const desc = contentEl.createEl("span", {text: 'Should the image be deleted, as it has been detected that it is being concurrently referenced by multiple documents? , The list of markdown documents that reference this image is as follows:'});
-        desc.addClass("image-log-desc");
 
         // 创建一个输入框
-        const logs = contentEl.createEl('textarea');
-        logs.addClass('image-state-logs');
+        const logs = contentEl.createEl('div');
+        logs.addClass('.fast-image-cleaner-log');
         logs.innerHTML = this.getLog();
+        
 
         // 创建两个按钮,确定和取消
         const buttonWrapper = contentEl.createEl('div');
         buttonWrapper.addClass('fast-image-cleaner-center-wrapper');
 
-        const cancelButton = buttonWrapper.createEl('button', { text: 'Cancel' });
+        const cancelButton = buttonWrapper.createEl('button', { text: 'close' });
 
         cancelButton.addClass('unused-images-button');
         cancelButton.addEventListener('click', () => {
